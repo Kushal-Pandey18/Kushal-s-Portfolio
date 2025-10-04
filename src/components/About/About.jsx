0ddc9daf106bab5a567ece3abac2596a314807b5
Kushal Pandey <kushal.pandey850@gmail.com>
2025-10-04 15:25:50 +0530

    resume update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1_pLl2wjYVCU-wnqXIhjhYr0YC0SJXvwv/view?usp=sharing"
+            href="https://drive.google.com/file/d/1xykWg8jkqPwG3Q0j5YBi9KYZJjxaCCKs/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
